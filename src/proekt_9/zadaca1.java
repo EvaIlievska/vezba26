7dc9837435d9e9e6f2741dcb1c3696a9bb2f3dc8
EvaIlievska <ilievska_eva@yahoo.com>
2024-02-11 09:30:47 +0100

    vezba26

---
+++ src/proekt_9/zadaca1.java
@@ -0,0 +1,11 @@
+package proekt_9;
+public class zadaca1 {
+
+		public static void main(String[] args) {
+			zadaca dog=new zadaca();
+			dog.bark();
+			zadaca resi=new zadaca();
+			resi.zadaca();
+		}
+
+	}
